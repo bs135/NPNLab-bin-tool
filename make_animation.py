8c41ab520e45db0ed1cd341dcc4b8cbb86f75c52
bs135 <thanhhai135@gmail.com>
2020-03-12 13:40:27 +0700

    update code

--- make_animation.py
+++ make_animation.py
@@ -6,7 +6,7 @@ G_MSTAR_TOOL_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def DoMakeAnimation(G_BRAND, G_LAUNCHER, G_VOICE, G_VERNUM, ANI_WIDTH, ANI_HEIGHT, ANI_COLOR, ANI_BACK):
     print("[i] Make bootanimation.zip")
-    ANI_FPS     = 20
+    ANI_FPS     = 10
     VERSION     = "{}.{}.{}{}".format(G_BRAND, G_LAUNCHER, G_VOICE, G_VERNUM)
     OUT_DIR     = G_MSTAR_TOOL_DIR + "/custom/bootanimation/" + G_BRAND + "_" + G_LAUNCHER + "/" + G_VOICE + str(G_VERNUM) + "/" + str(ANI_HEIGHT)
     IN_DIR      = G_MSTAR_TOOL_DIR + "/custom/bootanimation_src/" + G_BRAND + "/"
@@ -16,18 +16,13 @@ def DoMakeAnimation(G_BRAND, G_LAUNCHER, G_VOICE, G_VERNUM, ANI_WIDTH, ANI_HEIGH
     aniOptions = "-t {} -i {} -o {} -w {} -h {} -s {} --color {} --size {} --back {} --padx {} --pady {} -v".format(ver, IN_DIR, OUT_DIR, ANI_WIDTH, ANI_HEIGHT, ANI_FPS, ANI_COLOR, "11", ANI_BACK, padx, pady)
     bhelper.run_tool(G_MSTAR_TOOL_DIR + "/bin/bmc/bmc.exe", [aniOptions])
 
-#for vnum in range(500, 510):
-#    for voice in ["V", "N"]:
-        #DoMakeAnimation("UBC",     "UXBH", voice, vnum, 1920, 1080, "blue", "white")
-        #DoMakeAnimation("SANCO",   "UXCN", voice, vnum, 1920, 1080, "blue", "white")
-        # DoMakeAnimation("SANCO",   "UXCN", voice, vnum, 1920, 1080, "white", "#157dc4")
+for vnum in range(500, 510):
+    for voice in ["V", "N"]:
+        DoMakeAnimation("UBC",     "UXBH", voice, vnum, 1920, 1080, "white", "black")
+        #DoMakeAnimation("SANCO",   "UXCN", voice, vnum, 1920, 1080, "white", "black")
         #DoMakeAnimation("KOLIGHT", "UXCN", voice, vnum, 1920, 1080, "white","black")
 
-    # DoMakeAnimation("UBC",     "UXBH", "V", vnum, 1280, 720, "blue", "white")
-    # DoMakeAnimation("UBC",     "UXBH", "N", vnum, 1280, 720, "blue", "white")
-    # DoMakeAnimation("SANCO",   "UXCN", "V", vnum, 1280, 720, "blue", "white")
-    # DoMakeAnimation("SANCO",   "UXCN", "N", vnum, 1280, 720, "blue", "white")
-    # DoMakeAnimation("KOLIGHT", "UXCN", "V", vnum, 1280, 720, "white","black")
-    # DoMakeAnimation("KOLIGHT", "UXCN", "N", vnum, 1280, 720, "white","black")
+DoMakeAnimation("UBC",     "UXBH", 'V', 'TEST', 1920, 1080, "white", "black")
+#DoMakeAnimation("SANCO",   "UXCN", 'V', 'TEST', 1920, 1080, "white", "black")
 
-DoMakeAnimation("HBOX",   "DEMO", "V", "001", 1080, 1920, "white", "black")
+#DoMakeAnimation("HBOX",   "DEMO", "V", "001", 1080, 1920, "white", "black")
